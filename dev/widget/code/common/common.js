e973c5ce79e409159cd691790bc2c3badb964225
liuyanhao <374659635@qq.com>
2016-09-06 20:51:23 +0800

    基本完成 基础面板 组合面板 面板工具

---
+++ dev/widget/code/common/common.js
@@ -0,0 +1,44 @@
+/*
+	插件公共js
+*/
+;(function($,window,document,undefined){
+	$.extend({
+		// commonPluginInit: function (pluginName, $this, args, plugin, cb) {
+  //           var options = null;
+  //           if (args&&args.length>0) {
+  //               options = args[0];
+  //           }
+            
+  //           if (options === undefined || options === null || typeof options === 'object') {
+  //               //创建对象
+  //               return $this.each(function () {
+  //                   // if ($.data(this, 'plugin_' + pluginName)) {
+  //                   //     $.data(this, 'plugin_' + pluginName, cb(this));
+  //                   // }else{
+  //                   $.data(this, 'plugin_' + pluginName, null);
+  //                   $.data(this, 'plugin_' + pluginName, cb(this));
+  //                   // }
+  //               });
+  //           } else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+  //               //对象方法调用
+  //               var ret = null;
+  //               $this.each(function () {
+  //                   var instance = $.data(this, 'plugin_' + pluginName);
+  //                   if (!instance) {
+  //                       instance = $.data(this, 'plugin_' + pluginName, cb(this));
+  //                   }
+  //                   if (instance instanceof plugin && typeof instance[options] === 'function') {
+  //                       ret = instance[options].apply(instance, Array.prototype.slice.call(args, 1));
+  //                   } else {
+  //                       $.error('方法 ' + options + ' 不存在在插件 ' + pluginName);
+  //                       return this;
+  //                   }
+  //               });
+  //               return ret !== undefined ? ret : $this;
+  //           }
+  //       }
+  		pluginInit: function(pluginName,$this,args,plugin,func){
+
+  		}
+	})
+})(jQuery,window,document);
